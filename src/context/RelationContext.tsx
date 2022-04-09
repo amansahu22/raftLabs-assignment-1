f81abb85bd37a1b28afd9756ffcad227cbe87025
Aman Sahu <amansahu2019@iiitkottayam.ac.in>
2022-04-09 11:04:09 +0530

    Installed all the dependancies and initial setup

---
+++ src/context/RelationContext.tsx
@@ -0,0 +1,20 @@
+import React,{ useContext } from "react";
+
+// const RelationContext=React.createContext<{
+//     relations: Realtion[];
+//     addRelation: ()=>void;
+//     findRelation: ()=>void;
+// }>({
+//     relations:[],
+//     addRelation:() => void,
+//     findRelation : ()=>void
+// })
+
+// const RelationContextProvider: React.FC=(props)=>{
+//     return <RelationContext.Provider value={}>
+//     {props.children}
+//     </RelationContext.Provider>
+// }
+
+
+// export {RelationContext,RelationContextProvider};
